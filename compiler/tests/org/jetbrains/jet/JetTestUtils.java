a7a3656c5eec5c48d1c1bf36174c6ad79855c59d
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2013-11-01 17:28:05 +0400

    Add NotNull annotations

--- compiler/tests/org/jetbrains/jet/JetTestUtils.java
+++ compiler/tests/org/jetbrains/jet/JetTestUtils.java
@@ -95,6 +95,7 @@ public class JetTestUtils {
     public static final BindingTrace DUMMY_TRACE = new BindingTrace() {
 
 
+        @NotNull
         @Override
         public BindingContext getBindingContext() {
             return new BindingContext() {
@@ -155,6 +156,7 @@ public class JetTestUtils {
     };
 
     public static BindingTrace DUMMY_EXCEPTION_ON_ERROR_TRACE = new BindingTrace() {
+        @NotNull
         @Override
         public BindingContext getBindingContext() {
             return new BindingContext() {
